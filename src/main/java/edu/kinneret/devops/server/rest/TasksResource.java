51f3a11793b59d926a65f74faea8a743da1881b6
Shay Tsadok <shayts7@gmail.com>
2015-05-05 10:20:58 +0300

    Create Task Dao

--- src/main/java/edu/kinneret/devops/server/rest/TasksResource.java
+++ src/main/java/edu/kinneret/devops/server/rest/TasksResource.java
@@ -3,6 +3,7 @@ package edu.kinneret.devops.server.rest;
 import com.codahale.metrics.annotation.Timed;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Optional;
+import edu.kinneret.devops.server.dao.TaskDao;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -17,109 +18,37 @@ import java.util.concurrent.atomic.AtomicLong;
 @Path("/tasks")
 @Produces(MediaType.APPLICATION_JSON)
 public class TasksResource {
-    private final File tasksRepo;
-    private static AtomicInteger counter = new AtomicInteger(1);
 
-    public TasksResource(String repositoryBasePath) {
+    private TaskDao dao;
 
-        tasksRepo = new File(repositoryBasePath, "tasks");
-        if (!tasksRepo.exists())
-        {
-            try
-            {
-                tasksRepo.mkdir();
-            }
-            catch (Exception ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-        else
-        {
-            File[] tasks = tasksRepo.listFiles();
-            for(int i = 0; i < tasks.length; i++)
-            {
-                File task = tasks[i];
-                Integer taskId = Integer.parseInt(task.getName().split("\\.")[0]);
-                if (counter.get() < taskId+1)
-                    counter.set(taskId+1);
-            }
-        }
-
-        System.out.println("tasks counter is going to start from: " + counter.get());
+    public TasksResource(TaskDao dao) {
+        this.dao = dao;
     }
 
     @POST
     @Timed
     public Task createTask(Task taskToCreate)
     {
-        int id = counter.getAndIncrement();
-        taskToCreate.setId(id);
-
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.writeValue(new File(tasksRepo, id + ".json"), taskToCreate);
-            Task taskFromDisk = mapper.readValue(new File(tasksRepo, id + ".json"), Task.class);
-            return taskFromDisk;
-        }
-        catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
+        return dao.createTask(taskToCreate);
     }
 
     @GET
     @Timed
     public Collection<Task> getTasks() {
-        try {
-            ArrayList<Task> allTasks = new ArrayList<Task>();
-            ObjectMapper mapper = new ObjectMapper();
-            File[] tasks = tasksRepo.listFiles();
-
-            for (int i = 0; i < tasks.length; i++) {
-                Task taskFromDisk = mapper.readValue(tasks[i], Task.class);
-                allTasks.add(taskFromDisk);
-            }
-
-            return allTasks;
-        }
-        catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
+        return dao.getTasks();
     }
 
     @GET @Path("/{id}")
     @Timed
     public Task getSingleTask(@PathParam("id") String id)
     {
-        try
-        {
-            ObjectMapper mapper = new ObjectMapper();
-            Task taskFromDisk = mapper.readValue(new File(tasksRepo, id + ".json"), Task.class);
-            return taskFromDisk;
-        }
-        catch (Exception ex)
-        {
-            throw new RuntimeException(ex);
-        }
+        return dao.getSingleTask(id);
     }
 
     @DELETE @Path("/{id}")
     @Timed
     public Task deleteTask(@PathParam("id") String id)
     {
-        try
-        {
-            ObjectMapper mapper = new ObjectMapper();
-            File taskToDelete = new File(tasksRepo, id + ".json");
-            Task taskFromDisk = mapper.readValue(taskToDelete, Task.class);
-
-            taskToDelete.delete();
-
-            return taskFromDisk;
-
-        }
-        catch (Exception ex)
-        {
-            throw new RuntimeException(ex);
-        }
+        return dao.deleteTask(id);
     }
 }
